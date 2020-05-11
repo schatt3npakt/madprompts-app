bf259883c804a69322b72c0b44330ede439919ec
Luca La Furia <lucalafuria@hotmail.de>
2020-05-11 20:56:47 +0200

    started prototyping number input

--- src/components/TheAppView.vue
+++ src/components/TheAppView.vue
@@ -7,7 +7,24 @@
         />
 
         <div class="form">
-          <button>Challenge me!</button>
+          <button id="challenge">Challenge me!</button>
+
+          <div class="slider">
+            <label
+              class="slider__label"
+              for="adjectives"
+              >Adjectives</label>
+
+            <input
+              class="slider__input"
+              id="adjectives"
+              maxlength="1"
+              name="adjectives"
+              oninput="javascript: this.value=this.value.slice(0,this.maxLength)"
+              type="number"
+              value="2"
+            />
+          </div>
         </div>
       </div>
   </div>
@@ -39,28 +56,49 @@ export default Vue.extend({
 
         .logo {
           display: block;
+          filter: drop-shadow(0 5px 15px rgba(0, 0, 0, 0.3));
           margin: 0 auto 77px auto;
           max-height: 132px;
           max-width: 132px;
+          transition: filter 0.1s ease-out;
+
+          &:active {
+            filter: drop-shadow(0 0 0 rgb(0, 0, 0));
+            transform: scale(0.985);
+          }
         }
 
         .form {
+          button,
+          .slider {
+              border-radius: 15px;
+              box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+              color: white;
+              display: block;
+              font-size: 22px;
+              font-weight: 700;
+              height: 61px;
+              margin: margins.$mobile-btn-margin;
+              transition:
+                background-color 0.1s ease-out,
+                box-shadow 0.1s ease-out,
+                transform 0.1s ease-out;
+              width: 100%;
+
+              &:active,
+              &:focus {
+                outline: none;
+              }
+
+              &:active {
+                box-shadow: 0 0 0 rgb(0, 0, 0);
+                transform: scale(0.985);
+              }
+            }
+
           button {
             background-color: colors.$amethyst;
             border: none;
-            border-radius: 15px;
-            box-shadow: 0 5px 15px rgba(31, 6, 90, 0.3);
-            color: white;
-            display: block;
-            font-size: 22px;
-            font-weight: 700;
-            height: 61px;
-            margin: margins.$mobile-btn-margin;
-            transition:
-              background-color 0.1s ease-out,
-              box-shadow 0.1s ease-out,
-              transform 0.1s ease-out;
-            width: 100%;
 
             &:active,
             &:focus {
@@ -69,8 +107,33 @@ export default Vue.extend({
 
             &:active {
               background-color: colors.$wisteria;
-              box-shadow: 0 0 0 black;
-              transform: scale(0.985);
+            }
+          }
+
+          .slider {
+            background-color: colors.$concrete;
+            display: flex;
+            justify-content: space-between;
+            overflow: hidden;
+
+            &__input {
+              background-color: colors.$peter-river;
+              border: none;
+              color: white;
+              font-size: 22px;
+              font-weight: 700;
+              margin: 0;
+              padding: 0;
+              text-align: center;
+              width: 60px;
+            }
+
+            &__label {
+              display: block;
+              margin: 13px 0 0 0;
+              padding: 0;
+              text-align: center;
+              width: 100%;
             }
           }
         }
